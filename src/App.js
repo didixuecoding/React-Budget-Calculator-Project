ce922b877561645fa29f7736441e0ca4ede3066e
LearnToWin <60451278+didixuecoding@users.noreply.github.com>
2020-04-07 21:27:34 -0400

    Up to Lec185

--- src/App.js
+++ src/App.js
@@ -13,14 +13,63 @@ const initialExpense = [
 ];
 
 export default function App() {
+  // *** state values ***//
+  // all expense, add expense
   const [expenses, setExpenses] = useState(initialExpense);
+  // single expense
+  const [charge, setCharge] = useState("");
+  // single amount
+  const [amount, setAmount] = useState("");
+  // alert
+  const [alert, setAlert] = useState({ show: false });
+  // *** helper functions ***//
+  // handle Alert
+  const handleAlert = ({ type, text }) => {
+    setAlert({ show: true, type, text });
+    setTimeout(() => {
+      setAlert({ show: false });
+    }, 5000);
+  };
+  //handle charge
+  const handleCharge = e => {
+    console.log(`charge : ${e.target.value}`);
+    setCharge(e.target.value);
+  };
+  //handle amount
+  const handleAmount = e => {
+    console.log(`amount : ${e.target.value}`);
+    setAmount(e.target.value);
+  };
+  //handle submit
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (charge !== "" && amount > 0) {
+      const singleExpense = { id: uuid(), charge, amount };
+      setExpenses([...expenses, singleExpense]);
+      setCharge("");
+      setAmount("");
+      handleAlert({ type: "success", text: "item added" });
+    } else {
+      // handle alert call
+      handleAlert({
+        type: "danger",
+        text: `charge can't be empty value and amount has to be greater than zero`
+      });
+    }
+  };
 
   return (
     <>
-      <Alert />
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
       <h1>budget calculator</h1>
       <main className="App">
-        <ExpenseForm />
+        <ExpenseForm
+          charge={charge}
+          amount={amount}
+          handleAmount={handleAmount}
+          handleCharge={handleCharge}
+          handleSubmit={handleSubmit}
+        />
         <ExpenseList expenses={expenses} />
       </main>
       <h1>
@@ -28,7 +77,7 @@ export default function App() {
         <span className="total">
           $
           {expenses.reduce((total, expense) => {
-            return (total += expense.amount);
+            return (total += parseInt(expense.amount, 10));
           }, 0)}
         </span>
       </h1>
